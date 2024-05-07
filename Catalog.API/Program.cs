135a1c49d8b5e760b6ebe78822615f888fe1f72c
brenovandall <vbreno335@gmail.com>
2024-05-07 17:46:39 -0300

    Forced kestrel to use http port

--- Catalog.API/Program.cs
+++ Catalog.API/Program.cs
@@ -1,5 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.ConfigureKestrel(serverOptions =>
+{
+    // Docker = http://172.18.0.3:5000
+    serverOptions.ListenAnyIP(5000);
+});
+
 builder.Services.AddCarter();
 
 builder.Services.AddMediatR(config =>
