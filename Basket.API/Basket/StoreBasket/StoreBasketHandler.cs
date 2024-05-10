54e215e80599c134424a8f966c8bfc0289847676
brenovandall <vbreno335@gmail.com>
2024-05-10 16:32:09 -0300

    Still working on basket

--- Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++ Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,6 +1,23 @@
 namespace Basket.API.Basket.StoreBasket;
 
-public class StoreBasketHandler
+public record StoreBasketCommand(ShoppingCart Basket) : ICommand<StoreBasketResult>;
+public record StoreBasketResult(string username);
+
+public class StoreBasketCommandValidator : AbstractValidator<StoreBasketCommand> 
 {
-    
+    public StoreBasketHandler()
+    {
+        RuleFor(x => x.Basket).NotNull().WithMessage("Basket can not be null");
+        RuleFor(x => x.Basket.Username).NotEmpty().WithMessage("Username is required");
+    }
+}
+
+public async class StoreBasketHandler : ICommandHandler<StoreBasketCommand, StoreBasketResult>
+{
+    public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
+    {
+        ShoppingCart basket = command.Basket;
+
+        return new StoreBasketResult("sds");
+    }
 }
